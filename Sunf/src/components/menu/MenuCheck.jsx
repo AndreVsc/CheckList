e9f9ac5d3a3909af5870e9c2c2e7d975603a085e
Andre_Victor <95365578+AndreVsc@users.noreply.github.com>
2024-02-21 22:36:19 -0300

    0.0.1.3v

--- Sunf/src/components/menu/MenuCheck.jsx
+++ Sunf/src/components/menu/MenuCheck.jsx
@@ -2,24 +2,24 @@ import React,{useEffect,useState} from 'react';
 import Button from '../button/Button';
 import {AiOutlineLeft} from 'react-icons/ai';
 import './MenuCheck.css';
-
+import FormBlock from '../forms/FormBlock';
 
 export default function MenuCheck() {
 
-    const [block, setBlock] = useState([]);
+    const [collection, setCollection] = useState([]);
     const [show, setShow] = useState(false);
-    const [selectedBlockId, setSelectedBlockId] = useState(null);
+    const [selectedCollectionId, setSelectedCollectionId] = useState(null);
     const [id, setId] = useState(0);
 
     function showAll() {
-        setSelectedBlockId();
+        setSelectedCollectionId();
         setShow(true);
     }
     
     useEffect(() => {
         if (show) {
             return () => {
-                setSelectedBlockId(null);
+                setSelectedCollectionId(null);
                 setShow(false);
             };
         }
@@ -30,10 +30,10 @@ export default function MenuCheck() {
         <>
             <div className='containerMenu'>
                 <div className='menuPrincipal'>                
-                    {block==''?(<p className='textp'>Nenhum Produto</p>):(<p>Teste</p>)}
-
+                    {collection==''?(<p className='textp'>Nenhum Produto</p>):(<p>Teste</p>)}
                     <Button func={() => showAll()} value={<AiOutlineLeft fontSize={15} />} classN='bnt-b' />
                 </div>
+                {show && <FormBlock reset={setShow}/>}
             </div>
         </>
     );
